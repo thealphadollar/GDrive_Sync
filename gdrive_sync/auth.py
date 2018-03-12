a5c7dba0191bca4c5cf1cb9ce1f0d719581dabf8
Shivam Kumar Jha <shivam.cs.iit.kgp@gmail.com>
2018-03-12 10:44:12 +0530

    Update auth.py
    Add docstrings.

--- gdrive_sync/auth.py
+++ gdrive_sync/auth.py
@@ -12,14 +12,18 @@ try:
 except ImportError:
     from . import file_add
 
-# to handle authorisation of the user account
-
-
+    
 def drive_auth(reset):
-
+    """
+    asks for authentication to generate access token or uses existing access token
+    Args:
+        reset (bool): True for resetting linked account, bypasses existing access token.
+    Returns:
+        Google_Auth with permissions
+    """
     g_auth = GoogleAuth()
 
-    # changing default settings of GoogleAuth
+    # setting Google Auth for custom parameters
     g_auth.DEFAULT_SETTINGS['client_config_file'] = file_add.client_secrets
     g_auth.DEFAULT_SETTINGS['client_config_backend'] = 'file'
     g_auth.DEFAULT_SETTINGS['oauth_scope'] = ['https://www.googleapis.com/auth/drive']
@@ -30,7 +34,7 @@ def drive_auth(reset):
     g_auth.DEFAULT_SETTINGS['client_id'] = '442675981331-9sq75sq0731sc0pef1rsg2jksmqfov2f.apps.googleusercontent.com'
     g_auth.DEFAULT_SETTINGS['client_secret'] = 'bmPz_4djxl_NZsA4Cvvaz2XT'
 
-    # looking for saved authentication data
+    # if already authenticated, load file
     g_auth.LoadCredentialsFile(file_add.cred_file())
 
     if g_auth.credentials is None or reset:
@@ -51,10 +55,19 @@ def drive_auth(reset):
     return g_auth
 
 
-# to handle account reset or change
-
 def reset_account():
+    """
+    change linked account
+    Args:
+        None
+    Returns:
+        None
+    """
     if os.path.isfile(file_add.cred_file()):
         os.remove(file_add.cred_file())
 
-    drive_auth(1)
+    drive_auth(True)
+    
+ 
+if __name__ == '__main__':
+    pass
