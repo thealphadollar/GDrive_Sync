2ee44ab8e21e2a7879b1588b15a4bb9d9ec28abd
Shivam Kumar Jha <shivam.cs.iit.kgp@gmail.com>
2018-04-01 10:04:35 +0530

    Add Automatic Copy Function (#21)
    - Using the pyperclip module, now when the user asks manually for a share link, it is copied directly to the clipboard.
    - Version updated to 1.12

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name='drive_sync',  # name of the program
-    version='1.111',  # current version
+    version='1.12',  # current version
     description='GDrive_Sync is an automatic folder syncing client for Google Drive',
     long_description=readme(),
     classifiers=[
@@ -31,7 +31,8 @@ setup(
     install_requires=[
         'pydrive',
         'python-crontab',
-        'future'
+        'future',
+        'pyperclip'
       ],
     dependency_links=[],  # add github links of dependencies not present in pypi but have a setup file
     entry_points={
